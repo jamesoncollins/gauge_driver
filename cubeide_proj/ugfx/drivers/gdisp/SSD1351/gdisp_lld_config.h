69d1c3c4b02f33789f3c53fcaf38438c43ef1408
Jameson Collins <jameson.collins@gmail.com>
2024-03-03 14:50:05 -0500

    make ssd1351 support double buffering

--- cubeide_proj/ugfx/drivers/gdisp/SSD1351/gdisp_lld_config.h
+++ cubeide_proj/ugfx/drivers/gdisp/SSD1351/gdisp_lld_config.h
@@ -1,3 +1,4 @@
+
 /*
  * This file is subject to the terms of the GFX License. If a copy of
  * the license was not distributed with this file, you can obtain one at:
@@ -14,12 +15,19 @@
 /* Driver hardware support.                                                  */
 /*===========================================================================*/
 
-#define GDISP_HARDWARE_STREAM_WRITE		GFXON
-//#define GDISP_HARDWARE_STREAM_READ		GFXON
-//#define GDISP_HARDWARE_CONTROL			GFXON
+//#define GDISP_HARDWARE_STREAM_WRITE             GFXON
+#define GDISP_HARDWARE_FLUSH                    GFXON           // This controller requires flushing
+#define GDISP_HARDWARE_DRAWPIXEL                GFXON
+#define GDISP_HARDWARE_PIXELREAD                GFXON
+#define GDISP_HARDWARE_CONTROL          GFXON
+#define GDISP_HARDWARE_FILLS            GFXON
 
-#define GDISP_LLD_PIXELFORMAT			GDISP_PIXELFORMAT_RGB565
+#define GDISP_LLD_PIXELFORMAT                   GDISP_PIXELFORMAT_RGB565
 
-#endif	/* GFX_USE_GDISP */
+#define GDISP_CONTROL_INVERSE                   (GDISP_CONTROL_LLD+0)
 
-#endif	/* _GDISP_LLD_CONFIG_H */
+#endif  /* GFX_USE_GDISP */
+
+#endif  /* _GDISP_LLD_CONFIG_H */
+
+
