744e9e4a6a5315295ed1c247071eefe260501974
Jameson Collins <jameson.collins@gmail.com>
2024-03-17 07:22:12 -0400

    switch to ili9341

--- cubeide_proj/Core/Inc/board_ILI9341.h
+++ cubeide_proj/Core/Inc/board_ILI9341.h
@@ -38,6 +38,15 @@ extern DMA_HandleTypeDef hdma_spi2_tx;
 #define SET_CS SET_BIT(CS_PORT->ODR, CS_PIN)
 #define GET_CS READ_BIT(CS_PORT->IDR, CS_PIN)
 
+bool busy = false;
+uint8_t *data_ptr;
+uint32_t size_left = 0;
+
+bool bus_busy()
+{
+  return busy;
+}
+
 static GFXINLINE void init_board(GDisplay *g)
 {
 	(void) g;
@@ -96,10 +105,10 @@ static GFXINLINE void release_bus(GDisplay *g)
 	(void) g;
 }
 
-static GFXINLINE void write_index(GDisplay *g, gU16 index)
+static GFXINLINE void write_index(GDisplay *g, uint8_t index)
 {
   (void) g;
-
+  while(busy);
   while (HAL_SPI_GetState(&SPIDEV) != HAL_SPI_STATE_READY);
   CLR_DC;
   CLR_CS;
@@ -107,8 +116,9 @@ static GFXINLINE void write_index(GDisplay *g, gU16 index)
   SET_CS;
 }
 
-static GFXINLINE void write_data(GDisplay *g, gU16 data)
+static GFXINLINE void write_data_one(GDisplay *g, uint8_t data)
 {
+  while(busy);
   while (HAL_SPI_GetState(&SPIDEV) != HAL_SPI_STATE_READY);
   SET_DC;
   CLR_CS;
@@ -118,6 +128,50 @@ static GFXINLINE void write_data(GDisplay *g, gU16 data)
           1,
           HAL_MAX_DELAY);
   SET_CS;
+}
+
+
+
+/*
+ * WARNING: This is a DMA transfer. It will not set CS, that gets done in the
+ * callback.  DOn't use this for anything you need to do in a loop in the
+ * driver.
+ */
+static GFXINLINE void write_data(GDisplay *g, gU8* data, unsigned int length)
+{
+    (void) g;
+    busy = true;
+    while (HAL_SPI_GetState(&SPIDEV) != HAL_SPI_STATE_READY);
+    while (HAL_DMA_GetState(&hdma_spi2_tx) != HAL_DMA_STATE_READY);
+    SET_DC;
+    CLR_CS;
+    if(length>65536)
+      size_left = length-65535;
+    else
+      size_left = 0;
+    uint16_t xfer_len = (length>65535) ? 65535 : length;
+    data_ptr = data + xfer_len;
+    HAL_SPI_Transmit_DMA(
+            &SPIDEV,
+            (uint8_t *)data,
+            xfer_len );
+
+}
+
+void HAL_SPI_TxCpltCallback(SPI_HandleTypeDef *hspi)
+{
+  if(hspi->Instance==hspi2.Instance)
+  {
+    if(size_left)
+    {
+      write_data(NULL, data_ptr, size_left);
+    }
+    else
+    {
+      SET_CS;
+      busy = false;
+    }
+  }
 }
 
 static GFXINLINE void setreadmode(GDisplay *g)
