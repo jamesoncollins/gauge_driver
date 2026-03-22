072c187d5e6db4ee07dddc9d84f0ea9db89add60
Jameson Collins <jameson.collins@gmail.com>
2026-03-22 15:14:35 -0400

    more platform abtraction

--- cubeide_proj/Custom/Inc/x86_hal.h
+++ cubeide_proj/Custom/Inc/x86_hal.h
@@ -26,9 +26,24 @@ typedef struct { int _unused; } TIM_HandleTypeDef;
 typedef struct { int _unused; } UART_HandleTypeDef;
 typedef struct { int _unused; } RTC_HandleTypeDef;
 typedef struct { int _unused; } DMA_HandleTypeDef;
+typedef struct { int _unused; } GPIO_TypeDef;
+
+typedef int IRQn_Type;
+
+typedef enum
+{
+  GPIO_PIN_RESET = 0,
+  GPIO_PIN_SET
+} GPIO_PinState;
+
+#ifndef HAL_MAX_DELAY
+#define HAL_MAX_DELAY (0xFFFFFFFFU)
+#endif
 
 uint32_t HAL_GetTick(void);
 void HAL_Delay(uint32_t ms);
+void HAL_GPIO_WritePin(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin, GPIO_PinState PinState);
+GPIO_PinState HAL_GPIO_ReadPin(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin);
 
 #ifdef __cplusplus
 }
--- cubeide_proj/Custom/x86/x86_hal.cpp
+++ cubeide_proj/Custom/x86/x86_hal.cpp
@@ -15,3 +15,17 @@ extern "C" void HAL_Delay(uint32_t ms)
 {
   std::this_thread::sleep_for(std::chrono::milliseconds(ms));
 }
+
+extern "C" void HAL_GPIO_WritePin(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin, GPIO_PinState PinState)
+{
+  (void)GPIOx;
+  (void)GPIO_Pin;
+  (void)PinState;
+}
+
+extern "C" GPIO_PinState HAL_GPIO_ReadPin(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin)
+{
+  (void)GPIOx;
+  (void)GPIO_Pin;
+  return GPIO_PIN_RESET;
+}
